cf542fd5e662bef8092e0cc38b542e160510dfd2
Leo <ujfox13@gmail.com>
2021-12-16 00:49:37 +0300

    header in process

---
+++ note.js
@@ -0,0 +1,233 @@
+<div className="wrapper">
+                                                    <div className="header">
+                                                        <div className="headerContent">
+                                                            <div className="left"><h1><a href="#">Cherry</a></h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p></div>
+                                                            <div className="right">
+                                                                <form className="search">
+                                                                    <input type="text" placeholder="Search..."></input>
+                                                                    <input type="image" src="images/search.png" title="Search"></input>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+<div className="slider">
+    <div className="itemSlider">
+        <div className="bgSlide"><img src="images/bg-slide.jpg"></img></div>
+        <div className="descSlide">
+            <h1>Eaten berry</h1>
+            <p>and go for a walk</p>
+            <span>Duis aute irure dolor...</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+    </div>
+</div>
+<div className="nav">
+    <ul className="menu">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Sliders</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Styles</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Cherry</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</div>
+<div className="breadcrumbs"><a href="#">Home</a> / <a href="#">Another Page</a> / This page</div>
+<div className="main">
+    <div className="leftCol">
+        <form>
+            <h2>Contact us</h2>
+            <input type="text" placeholder="Lorem ipsum">
+            <input type="text" placeholder="Dolor sit">
+            <input type="text" disabled="disabled" placeholder="Disabled">
+            <input type="text" placeholder="Consectetur">
+            <select>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+            </select>
+            <div className="row">
+                <h4>Radio</h4>
+                <input type="radio"></input>
+                <label>Lorem ipsum dolor sit amet</label>
+                <div className="clear"></div>
+                <input type="radio"></input>
+                <label>Duis aute irure dolor</label>
+                <div className="clear"></div>
+                <input type="radio"></input>
+                <label>Excepteur sint occaecat</label>
+                <div className="clear"></div>
+            </div>
+            <div className="row">
+                <h4>Checkbox</h4>
+                <input type="checkbox" />input
+                <label>Duis aute irure dolor</label>
+                <div className="clear"></div>
+                <input type="checkbox" />
+                <label>Excepteur sint occaecat</label>
+                <div className="clear"></div>
+            </div>
+            <button type="submit" className="minWidth">Lorem ipsum</button>
+            <button type="reset" className="minWidth">Reset</button>
+        </form>
+        <div className="row">
+            <h2>Headline</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p><a href="#">More about</a></p>
+        </div>
+    </div>
+    <div className="rightCol">
+        <h1>What We Do…</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in <a href="#">voluptate velit esse cillum dolore</a> eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <ul className="img">
+            <li><a href="#"><img src="images/img1.png"></a></li>
+            <li><a href="#"><img src="images/img2.png"></a></li>
+            <li><a href="#"><img src="images/img3.png"></a></li>
+        </ul>
+        <h1>Lists</h1>
+        <div className="row">
+            <div className="span">
+                <h4>List style type circle <code>&lt;ul&gt;</code></h4>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet</li>
+                    <li>Sed ut perspiciatis unde</li>
+                    <li>At vero eos et accusamus
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Sed ut perspiciatis unde</li>
+                            <li>At vero eos et accusamus</li>
+                        </ul>
+                    </li>
+                    <li>Lorem ipsum dolor sit amet</li>
+                    <li>Sed ut perspiciatis unde</li>
+                    <li>At vero eos et accusamus</li>
+                </ul>
+            </div>
+            <div className="span">
+                <h4>Unstyled <code>&lt;ul className="unstyled"&gt;</code></h4>
+                <ul className="unstyled">
+                    <li>Lorem ipsum dolor sit amet</li>
+                    <li>Sed ut perspiciatis unde</li>
+                    <li>At vero eos et accusamus
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Sed ut perspiciatis unde</li>
+                            <li>At vero eos et accusamus</li>
+                        </ul>
+                    </li>
+                    <li>Lorem ipsum dolor sit amet</li>
+                    <li>Sed ut perspiciatis unde</li>
+                    <li>At vero eos et accusamus</li>
+                </ul>
+            </div>
+            <div className="span">
+                <h4>Ordered <code>&lt;ol&gt;</code></h4>
+                <ol>
+                    <li>Lorem ipsum dolor sit amet</li>
+                    <li>Sed ut perspiciatis unde</li>
+                    <li>At vero eos et accusamus</li>
+                    <li>Lorem ipsum dolor sit amet</li>
+                    <li>Sed ut perspiciatis unde</li>
+                    <li>At vero eos et accusamus</li>
+                </ol>
+            </div>
+        </div>
+        <h1>Tables</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Lorem</th>
+                    <th>Ipsum</th>
+                    <th>Dolor</th>
+                    <th>Sit</th>
+                    <th>Amet</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Lorem ipsum</td>
+                    <td>At vero eos</td>
+                    <td>Sed ut</td>
+                    <td>Dolor sit amet</td>
+                    <td><a href="#">Epd.</a></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Sed ut</td>
+                    <td>Dolor sit amet</td>
+                    <td>Lorem ipsum</td>
+                    <td>At vero eos</td>
+                    <td><a href="#">Epd.</a></td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>At vero eos</td>
+                    <td>Lorem ipsum</td>
+                    <td>Sed ut</td>
+                    <td>Dolor sit amet</td>
+                    <td><a href="#">Epd.</a></td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Dolor sit amet</td>
+                    <td>At vero eos</td>
+                    <td>Lorem ipsum</td>
+                    <td>Sed ut</td>
+                    <td><a href="#">Epd.</a></td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>At vero eos</td>
+                    <td>Lorem ipsum</td>
+                    <td>Sed ut</td>
+                    <td>Dolor sit amet</td>
+                    <td><a href="#">Epd.</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<div className="footer">
+    <div className="footerContent">
+        <div className="span1">
+            <h1>Sed ut</h1>
+            <div className="borderBottom"></div>
+            <p>Lorem ipsum dolor sit</p>
+            <ul className="clock">
+                <li>Nights! Absolutely No Extra Charge</li>
+                <li>Weekends! Absolutely No Extra Charge</li>
+                <li>Holidays! Absolutely No Extra Charge</li>
+            </ul>
+            <div className="social">
+                <div>Мы в социальных сетях: </div><ul><li><a target="newtab" href="http://www.facebook.com/?sk=app_2309869772"><img src="images/facebook.png"></a></li><li><a target="newtab" href="https://twitter.com/psdhtmlcss"><img src="images/twitter.png" /></a></li><li><img src="images/vk.png" /></li></ul>
+            </div>
+        </div>
+        <div className="span1">
+            <h1>At vero eos</h1>
+            <div className="borderBottom"></div>
+            <p>Lorem ipsum dolor sit</p>
+            <ul>
+                <li>West Hollywood (323) 221-1107</li>
+                <li>Beverly Hills (310) 202-5428</li>
+                <li>Pasadena (626) 296-2664</li>
+                <li>West Hollywood (323) 221-1107</li>
+                <li>Beverly Hills (310) 202-5428</li>
+            </ul>
+            <p><strong>Lorem ipsum dolor sit amet</strong></p>
+        </div>
+        <div className="span1">
+            <h1>Lorem ipsum dolor</h1>
+            <div className="borderBottom"></div>
+            <p>Lorem ipsum dolor sit</p>
+            <ul className="unstyled">
+                <li>Hi-Tech Cherry Company</li>
+                <li><a href="mailto:psdhtmlcss@mail.ru">infocherry@gmail.com</a></li>
+                <li>5104 W. Washington Blvd</li>
+                <li>Los Angeles , CA , 90016 United States</li>
+            </ul>
+        </div>
+    </div>
+</div>
+</div>
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="header">
+            <div className="headerContent">
+                <div className="left"><h1><a href="#">Cherry</a></h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p></div>
+                <div className="right">
+                    <form className="search">
+                        <input type="text" placeholder="Search..."></input>
+                        <input type="image" src="images/search.png" title="Search"></input>
+                    </form>
+                </div>
+            </div>
         </div>
+
     );
 };
 
